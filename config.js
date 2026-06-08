6847d73afcd604917fdb4524b9c58a6c51ff4e81
eleighma <eleighma@uvm.edu>
2026-06-07 23:46:58 -0400

    Update config.js

--- config.js
+++ config.js
@@ -33,9 +33,9 @@ const CONFIG = {
             nextBtn: "Next ❤️"                                         // Text for the next button
         },
         third: {
-            text: "Will you be my Valentine on February 14th, 2025? 🌹", // The big question!
-            yesBtn: "Yes!",                                             // Text for "Yes" button
-            noBtn: "No"                                                 // Text for "No" button
+            text: "Well we all know that I love you more, right?, // The big question!
+            yesBtn: "Obviously, duh",                                             // Text for "Yes" button
+            noBtn: "Nuh uh"                                                 // Text for "No" button
         }
     },
 
@@ -57,11 +57,11 @@ const CONFIG = {
     // Color scheme for the website
     // Use https://colorhunt.co or https://coolors.co to find beautiful color combinations
     colors: {
-        backgroundStart: "#ffafbd",      // Gradient start (try pastel colors for a soft look)
-        backgroundEnd: "#ffc3a0",        // Gradient end (should complement backgroundStart)
-        buttonBackground: "#ff6b6b",     // Button color (should stand out against the background)
-        buttonHover: "#ff8787",          // Button hover color (slightly lighter than buttonBackground)
-        textColor: "#ff4757"             // Text color (make sure it's readable!)
+        backgroundStart: "#5E0006",      // Gradient start (try pastel colors for a soft look)
+        backgroundEnd: "#5E0006",        // Gradient end (should complement backgroundStart)
+        buttonBackground: "#EED9B9",     // Button color (should stand out against the background)
+        buttonHover: "#EED9B9",          // Button hover color (slightly lighter than buttonBackground)
+        textColor: "#D53E0F"             // Text color (make sure it's readable!)
     },
 
     // Animation settings
